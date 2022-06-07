dca0c03290326bea11999622b1da3141867143ba
Kestrel.Bryce <99274358+KestrelBryce@users.noreply.github.com>
2022-06-07 18:04:54 +0000

    Finished CPT-08

--- js/gameScene.js
+++ js/gameScene.js
@@ -4,6 +4,11 @@
 class GameScene extends Phaser.Scene {
   constructor () {
     super({ key: 'gameScene' })
+
+    // variable holding game scene background
+    this.background = null
+    // variable holding spaceship
+    this.ship = null
   }
 
   // background colour in rgb
@@ -14,10 +19,22 @@ class GameScene extends Phaser.Scene {
   // print scene to console for debugging
   preload () {
     console.log('Game Scene')
+
+    // images
+    // load star background image
+    this.load.image('starBackground', 'images/starBackground.png')
+    // load ship image
+    this.load.image('ship', 'images/spaceShip.png')
   }
 
   create (data) {
-    
+    // load background
+    this.background = this.add.image(0, 0, 'starBackground').setScale(2.0)
+    // place background
+    this.background.setOrigin(0,0)
+
+    // adding ship physics
+    this.ship = this.physics.add.sprite(1920 / 2, 1080 - 100, 'ship')
   }
 
   // switching to the game scene
